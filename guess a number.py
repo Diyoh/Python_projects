2c654b67b2d88509e1c4428bf0535b2506215725
Diyoh Shiloh <95603105+Diyoh@users.noreply.github.com>
2022-08-23 09:26:13 -0700

    Update guess a number.py

--- guess a number.py
+++ guess a number.py
@@ -1,20 +1,24 @@
 from art import logo
 import random
 from replit import clear
+
 BEGINNER = 10
 AMATEUR = 8
 PROFFESIONAL = 6
 SUPERSTAR = 4
 number_range = []
 lives = 0
-for i in range(1,101):
-  number_range.append(i)
+for i in range(1, 101):
+    number_range.append(i)
 print(number_range)
 number = 0
+
+
 def choose_number():
-  """returns a random number"""
-  number = random.choice(number_range)
-  return number
+    """returns a random number"""
+    number = random.choice(number_range)
+    return number
+
 
 # def choose_level():
 #   """asigns the number of lives with respect to the level the player choses"""
@@ -28,42 +32,53 @@ def choose_number():
 #   elif chose_level == "superstar":
 #     lives = SUPERSTAR
 #   return lives
-  
- 
 
-  
-def game():
-  print(logo)
-  print("Welcome to my guess a number game")
-  print("i am thinking of a number between 1-100")
-  number = choose_number()
-  chose_level = input("choose a level of difficulty. Type: 'beginner', 'Amateur', 'professional', 'superstar'\n\n").lower()
-  if chose_level == "beginner":
-    lives = BEGINNER
-  elif chose_level == "amateur":
-    lives = AMATEUR
-  elif chose_level == "professional":
-    lives = PROFFESIONAL
-  elif chose_level == "superstar":
-    lives = SUPERSTAR
-  else:
-    print("howdy you missed something ")
 
-  can_guess = True
-  while can_guess:
-    print(f"you have {lives} attempts remaining to guess the number")
-    guess = int(input("make a guess:"))
-    if guess > number:
-      lives -= 1
-      if lives == 0:
-         can_guess = False
-      print(f"Too high.\nGuess again.\nYou have {lives} attempts remaining to guess the number")
-    elif guess < number:
-       lives -=1
-       if lives == 0:
-         can_guess = False
-       print(f"Too low.\nGuess again.\nYou have {lives} attempts remaining to guess the number.")
-    elif guess == number:
-       print(f"Yes! you won the correct number is {number}")
- 
-game()  
+def play_again():
+    """restarts or ends the game"""
+    continue_playing = input("type 'p' too continue playing or 'q' to quit: ").lower()
+    if continue_playing == "p":
+        clear()
+        game(lives)
+    else:
+        clear()
+
+
+def game(lives):
+    print(logo)
+    print("Welcome to my guess a number game")
+    print("i am thinking of a number between 1-100")
+    number = choose_number()
+    chose_level = input(
+        "choose a level of difficulty. Type: 'beginner', 'Amateur', 'professional', 'superstar'\n\n"
+    ).lower()
+    if chose_level == "beginner":
+        lives = BEGINNER
+    elif chose_level == "amateur":
+        lives = AMATEUR
+    elif chose_level == "professional":
+        lives = PROFFESIONAL
+    elif chose_level == "superstar":
+        lives = SUPERSTAR
+    else:
+        print("howdy you missed something ")
+
+    while lives > 0:
+        print(f"you have {lives} attempts remaining to guess the number")
+        guess = int(input("make a guess:"))
+        if guess > number:
+            lives -= 1
+            print(
+                f"Too high.\nGuess again.\nYou have {lives} attempts remaining to guess the number"
+            )
+        elif guess < number:
+            lives -= 1
+            print(
+                f"Too low.\nGuess again.\nYou have {lives} attempts remaining to guess the number."
+            )
+        elif guess == number:
+            print(f"Yes! you won.The correct number is {number}")
+            play_again()
+    play_again()
+
+game(lives)
