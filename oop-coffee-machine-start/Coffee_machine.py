38e5d36fc18b4c8d6e5b737cc50ab655959b7b4d
Diyoh Shiloh <95603105+Diyoh@users.noreply.github.com>
2022-12-10 10:15:14 -0800

    Update Coffee_machine.py

--- oop-coffee-machine-start/Coffee_machine.py
+++ oop-coffee-machine-start/Coffee_machine.py
@@ -1,13 +1,12 @@
-import menu
 from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
-my_coffee_machine = CoffeeMaker()
-money = MoneyMachine()
-# my_menu = MenuItem()
+money_machine = MoneyMachine()
+coffee_maker = CoffeeMaker()
 menu = Menu()
 is_on = True
+
 
 while is_on:
     options = menu.get_items()
@@ -23,15 +22,3 @@ while is_on:
         is_enough_money = money_machine.make_payment(drink.cost)
         if is_enough_money and is_enough_ingredients:
             coffee_maker.make_coffee(drink)
-
-
-
-
-
-
-
-
-
-
-
-
