b16e036f259d3a62a92bca94a9ce911bbd2c2d53
Diyoh Shiloh <95603105+Diyoh@users.noreply.github.com>
2022-12-10 10:13:01 -0800

    Create Coffee_machine.py

--- oop-coffee-machine-start/Coffee_machine.py
+++ oop-coffee-machine-start/Coffee_machine.py
@@ -11,16 +11,27 @@ is_on = True
 
 while is_on:
     options = menu.get_items()
-    choice = (input(f"what will you like {options}  "))
-    if choice == "off":
+    choice = input(f"what will you like? {options}: ")
+    if choice.lower() == "off":
         is_on = False
-    elif choice == "report":
-        my_coffee_machine.report()
-        money.report()
-
+    elif choice.lower() == "report":
+        coffee_maker.report()
+        money_machine.report()
     else:
         drink = menu.find_drink(choice)
-        if my_coffee_machine.is_resource_sufficient(drink):
-            if money.make_payment(drink.cost):
-                my_coffee_machine.make_coffee(drink)
+        is_enough_ingredients = coffee_maker.is_resource_sufficient(drink)
+        is_enough_money = money_machine.make_payment(drink.cost)
+        if is_enough_money and is_enough_ingredients:
+            coffee_maker.make_coffee(drink)
+
+
+
+
+
+
+
+
+
+
+
 
